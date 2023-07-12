ef6c9d7ced8c7429ae051a9224d77b5dc1fec2cf
MariamO6274 <ostatishvili@gmail.com>
2023-07-12 00:07:17 -0600

    Adding fonts

--- src/FakeStoreApiApp.js
+++ src/FakeStoreApiApp.js
@@ -3,10 +3,9 @@ import { useEffect } from "react";
 import ProductsCards from "./ProductsCards";
 import "./App.css";
 
-// You’ll get this 20 products, out of this 20 products, you show all this 20 products in cards,
-// You create a drop down for addending and dissenting orders..sorting
-
 const FakeStoreApiApp = () => {
+
+
   const [products, setproducts] = useState([]);
 
   useEffect(() => {
@@ -15,25 +14,30 @@ const FakeStoreApiApp = () => {
         .then((res) => res.json())
         .then((json) => {
           console.log(json);
-          setproducts(json); // save 20 obj to the state for re-render
+          setproducts(json);
         });
     }, 2000);
   }, []);
 
   return (
     <>
-      <ProductsCards />
+      {/* <ProductsCards
+        title=""
+        description
+        image
+      /> */}
       <div className="container">
         {products.map((product) => {
           return (
             <>
               <div className="cards">
                 <div className="card-content">
+                  <img src={product.image} />
                   <h3>{product.title}</h3>
+                  <h4>$ {product.price}</h4>
+                  <hr />
                   <p>{product.description}</p>
-                  <h3>$ {product.price}</h3>
                 </div>
-                <img src={product.image} />
               </div>
             </>
           );
--- src/ProductsCards.js
+++ src/ProductsCards.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 const ProductsCards = (props) => {
-      const { title, imageUrl } = props;
-  return <></>;
+  const { title, image, description, price } = props;
+  const [products, setproducts] = useState([]);
+
+    useEffect(() => {
+      setTimeout(() => {
+        fetch("https://fakestoreapi.com/products")
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+            setproducts(json);
+          });
+      }, 2000);
+    }, []);
+  return (
+    <>
+      <div className="container">
+        {products.map((product) => {
+          return (
+            <>
+              <div className="cards">
+                <div className="card-content">
+                  <h3>{product.title}</h3>
+                  <hr />
+                  <p>{description}</p>
+                  <h3>$ {price}</h3>
+                </div>
+                <img src={image} />
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default ProductsCards;
-
-
-
-/**
- * I should have 20 cards, for that I need to create render list of 20 cards,
- * and each card should be passed with props, prop of Title and image
- * Inside my <Card/> I will render title along with some border
- */
